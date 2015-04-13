821842a06817f1e4b5974759be39163768e00c6e
= <=>
2015-04-13 15:13:32 +0800

    modify email format

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,14 +1,14 @@
 class AppMailer < ActionMailer::Base
   def send_welcome_email(user)
     @user = user #@user instance variable is used in email's view template
-    mail to: "#{@user.email} <#{@user.full_name}>", from: 'info@myflix.com <MyFlix>', subjet: "Welcome to MyFlix"
+    mail to: "#{@user.email} <#{@user.full_name}>", from: 'MyFlix <info@myflix.com>', subjet: "Welcome to MyFlix"
   end
   def send_forget_password_email(email)
     @user = User.where(email: email).last
-    mail to: "#{@user.email} <#{@user.full_name}>", from: 'info@myflix.com <MyFlix>', subject: "Reset your password."
+    mail to: "#{@user.email} <#{@user.full_name}>", from: 'MyFlix <info@myflix.com>', subject: "Reset your password."
   end
   def send_invitation_email(invitation)
     @invitation = invitation
-    mail to: "#{@invitation.recipient_name} <#{@invitation.recipient_email}>", from: "#{@invitation.inviter.full_name} <#{@invitation.inviter.email}>", subject: "Invitaion to register an MyFlix member today."
+    mail to: "#{@invitation.recipient_email} <#{@invitation.recipient_name}>", from: "#{@invitation.inviter.full_name} <#{@invitation.inviter.email}>", subject: "Invitaion to register an MyFlix member today."
   end
 end
