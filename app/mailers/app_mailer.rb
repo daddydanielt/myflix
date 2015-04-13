7264e0cd4c21da72209190474ea01bbf69b3a074
= <=>
2015-04-13 15:08:13 +0800

    modify email format

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,11 +1,11 @@
 class AppMailer < ActionMailer::Base
   def send_welcome_email(user)
     @user = user #@user instance variable is used in email's view template
-    mail to: user.email, from: 'info@myflix.com', subjet: "Welcome to MyFlix"
+    mail to: "#{@user.email} <#{@user.full_name}>", from: 'info@myflix.com <MyFlix>', subjet: "Welcome to MyFlix"
   end
   def send_forget_password_email(email)
     @user = User.where(email: email).last
-    mail to: email, from: 'info@myflix.com', subject: "Reset your password."
+    mail to: "#{@user.email} <#{@user.full_name}>", from: 'info@myflix.com <MyFlix>', subject: "Reset your password."
   end
   def send_invitation_email(invitation)
     @invitation = invitation
