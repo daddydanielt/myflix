66ad84e5c093089e2e1ec626bff07afbe93fc182
= <=>
2015-04-13 16:10:59 +0800

    fix error

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -3,17 +3,20 @@ class AppMailer < ActionMailer::Base
     @user = user #@user instance variable is used in email's view template
     #mail to: '#{@user.email}', from: 'info@myflix.com', subjet: "Welcome to MyFlix"
     mail to: email_with_name(@user.full_name, @user.email),
-         from: email_with_name('MyFlix','info@myflix.com'), subjet: "Welcome to MyFlix."
+         from: email_with_name('MyFlix','info@myflix.com'),
+         subjet: "Welcome to MyFlix."
   end
   def send_forget_password_email(email)
     @user = User.where(email: email).last
     mail to: email_with_name(@user.full_name, @user.email),
-         from: email_with_name('MyFlix','info@myflix.com'), subject: "Reset your password."
+         from: email_with_name('MyFlix','info@myflix.com'),
+         subject: "Reset your password."
   end
   def send_invitation_email(invitation)
     @invitation = invitation
     mail to: email_with_name(@invitation.recipient_name,@invitation.recipient_email),
-         from: email_with_name(@invitation.inviter.full_name}, @invitation.inviter.email), subject: "Invitaion to register an MyFlix member today."
+         from: email_with_name(@invitation.inviter.full_name, @invitation.inviter.email),
+         subject: "Invitaion to register an MyFlix member today."
   end
 
   private
