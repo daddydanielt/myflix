feac2524d48555e85de82a6c3a15dd416a4d9156
= <=>
2015-04-17 14:22:56 +0800

    ok

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,4 +40,8 @@ Myflix::Application.configure do
   :domain         => 'myflix-test.herokuapp.com',
   :authentication => :plain,
   }
+
+  # for Sentry-Raven
+  # In Rails, all uncaught exceptions will be automatically reported.
+  config.action_dispatch.show_exceptions = false # this is the default setting in production
 end
