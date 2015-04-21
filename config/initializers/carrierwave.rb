b55de1581d73fb03bbba78a69a19f6f660c1c3a5
= <=>
2015-04-22 01:00:47 +0800

    ok

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -23,6 +23,7 @@ CarrierWave.configure do |config|
       access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
+    
     #--->
   else
     config.storage = :file
